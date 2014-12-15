61656f1cba9d2de7113d652c755ae4f1fad0e607
Solomon Hawk <solomon.hawk@viget.com>
2014-12-15 00:13:23 -0500

    Update Sortable component, fix key access

--- src/javascripts/mixins/Sortable.js
+++ src/javascripts/mixins/Sortable.js
@@ -1,12 +1,24 @@
 var Sortable = {
   getDefaultProps: function() {
     return {
-      "data-id" : this.props.key,
-      draggable : true,
-      onDragEnd: this.sortEnd.bind(this),
-      onDragOver: this.dragOver.bind(this),
-      onDragStart: this.sortStart.bind(this)
+      draggable : true
     }
+  },
+
+  componentDidMount: function() {
+    var el = this.getDOMNode();
+    el.setAttribute('data-id', this._currentElement.key);
+    el.ondragend = this.sortEnd;
+    el.ondragover = this.dragOver;
+    el.ondragstart = this.sortStart;
+  },
+
+  componentWillUnmount: function() {
+    var el = this.getDOMNode();
+    el.removeAttribute('data-id');
+    el.ondragend = null;
+    el.ondragover = null;
+    el.ondragstart = null;
   },
 
   update: function(to, from) {
@@ -24,6 +36,7 @@ var Sortable = {
       e.currentTarget.dataset.id :
       e.currentTarget.getAttribute('data-id');
     e.dataTransfer.effectAllowed = 'move';
+
     try {
       e.dataTransfer.setData('text/html', null);
     } catch (ex) {
@@ -31,17 +44,17 @@ var Sortable = {
     }
   },
 
-  move: function(over,append) {
+  move: function(over, append) {
     var to = Number(over.dataset.id);
     var from = this.props.data.dragging != undefined ? this.props.data.dragging : Number(this.dragged);
     if(append) to++;
     if(from < to) to--;
-    this.update(to,from);
+    this.update(to, from);
   },
 
   dragOver: function(e) {
     e.preventDefault();
-    var over = e.currentTarget
+    var over = e.currentTarget;
     var relX = e.clientX - over.getBoundingClientRect().left;
     var relY = e.clientY - over.getBoundingClientRect().top;
     var height = over.offsetHeight / 2;
@@ -50,6 +63,8 @@ var Sortable = {
   },
 
   isDragging: function() {
-    return this.props.data.dragging == this.props.key
+    return this.props.data.dragging == this._currentElement.key
   }
 }
+
+module.exports = Sortable;
